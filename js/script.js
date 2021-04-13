64e7c2fb5a51046b16aeb25434b48adb98b6bfe9
ammduncan <ammduncan@gmail.com>
2021-04-13 20:26:49 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const latestArticles = document.querySelector(".latest-content .articles");
+const shiftLeft = document.querySelector(".shift__btn--left");
+const shiftRight = document.querySelector(".shift__btn--right");
+
+const displayAppropriateBtns = () => {
+  if (latestArticles.scrollLeft > 0) {
+    shiftLeft.style.display = "block";
+  } else {
+    shiftLeft.style.display = "none";
+  }
+  if (latestArticles.scrollLeft >= latestArticles.scrollLeftMax - 40) {
+    shiftRight.style.display = "none";
+  } else {
+    shiftRight.style.display = "block";
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  displayAppropriateBtns();
+
+  latestArticles.addEventListener("scroll", function () {
+    displayAppropriateBtns();
+  });
+
+  shiftLeft.addEventListener("click", function () {
+    latestArticles.scrollTo({
+      left: latestArticles.scrollLeft - 0.8 * window.innerWidth,
+      behavior: "smooth",
+    });
+    // latestArticles.scrollLeft -= window.innerWidth;
+  });
+
+  shiftRight.addEventListener("click", function () {
+    latestArticles.scrollTo({
+      left:
+        latestArticles.scrollLeft +
+        (window.innerWidth > 640 ? 0.85 : 0.95) * window.innerWidth,
+      behavior: "smooth",
+    });
+  });
+});
