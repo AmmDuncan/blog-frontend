d5f667ea732bbe89078fce8ad0e3dcde05499e9c
ammduncan <ammduncan@gmail.com>
2021-04-13 20:48:44 +0000

    made scroll mandatory

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
     latestArticles.scrollTo({
       left:
         latestArticles.scrollLeft -
-        (window.innerWidth > 704 ? 0.8 * window.innerWidth : 352),
+        (window.innerWidth > 704 ? 0.8 * window.innerWidth : 344),
       behavior: "smooth",
     });
     // latestArticles.scrollLeft -= window.innerWidth;
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
     latestArticles.scrollTo({
       left:
         latestArticles.scrollLeft +
-        (window.innerWidth > 704 ? 0.8 * window.innerWidth : 352),
+        (window.innerWidth > 704 ? 0.8 * window.innerWidth : 344),
       behavior: "smooth",
     });
   });
