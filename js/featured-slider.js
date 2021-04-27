8be87b25b16b8f11d2b80bb043fff25f7bfd5231
ammduncan <ammduncan@gmail.com>
2021-04-27 16:57:31 +0000

    changed featured articles on mobile

---
+++ js/featured-slider.js
@@ -0,0 +1,20 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+    const featured_slider_items = document.querySelectorAll('.featured .controller .item')
+    const featured_article_items = document.querySelectorAll('.featured .articles .article')
+    const featured_articles = document.querySelector('.featured .articles')
+
+    featured_articles.addEventListener('scroll', function () {
+        const startingPoint = this.getBoundingClientRect().left
+        featured_article_items.forEach((item, index) => {
+            let itemLeft = item.getBoundingClientRect().left
+            if(itemLeft >= startingPoint
+               && itemLeft < startingPoint + 100) {
+                featured_slider_items.forEach(button => {
+                    button.classList.remove('active')
+                })
+                featured_slider_items[index].classList.add('active')
+            }
+        })
+    })
+})
