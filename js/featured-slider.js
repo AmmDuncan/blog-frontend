981669ed2d2702582d36ef10f85d85be2227dc7f
ammduncan <ammduncan@gmail.com>
2021-04-28 11:08:46 +0000

    changed featured articles on mobile

--- js/featured-slider.js
+++ js/featured-slider.js
@@ -7,18 +7,23 @@ document.addEventListener('DOMContentLoaded', function() {
     featured_articles.addEventListener('scroll', function () {
         const startingPoint = this.getBoundingClientRect().left
         const fullWidth = this.getBoundingClientRect().width
-        featured_article_items.forEach((item, index) => {
-            function getAndCheck() {
-                let itemLeft = item.getBoundingClientRect().left
-                if(itemLeft >= startingPoint
-                   && itemLeft < startingPoint + .5 * fullWidth) {
-                    featured_slider_items.forEach(button => {
-                        button.classList.remove('active')
-                    })
-                    featured_slider_items[index].classList.add('active')
+
+        setTimeout(function(){
+            featured_article_items.forEach((item, index) => {
+                function getAndCheck() {
+                    let itemLeft = item.getBoundingClientRect().left
+                    if (itemLeft >= startingPoint
+                        && itemLeft < startingPoint + .5 * fullWidth) {
+                        featured_slider_items.forEach(button => {
+                            button.classList.remove('active')
+                        })
+                        featured_slider_items[index].classList.add('active')
+                    }
                 }
-            }
-            setTimeout(getAndCheck, 200)
-        })
+
+                getAndCheck()
+            })
+        }, 200)
     })
+
 })
